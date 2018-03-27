3dec6996b6606b42eb36680d382d4f55aa09ddbf
Tim Head <betatim@gmail.com>
2018-03-27 12:38:09 +0200

    Upgrade to JHub chart v0.6

--- deploy.py
+++ deploy.py
@@ -4,9 +4,3 @@
 # helm repo update
 # (cd ohjh && helm dep up)
 # helm upgrade --install --namespace jhub ohjh ohjh --version=v0.1.0 -f secrets.yaml
-
-
-
-
-# helm upgrade ohjhub jupyterhub/jupyterhub --version=v0.6.0-baa1618 -f config.yaml -f secrets.yaml
-#
