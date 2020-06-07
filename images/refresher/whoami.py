f83bea7eb95bb6ab933456b5a78f9d8e0437d6b1
Tim Head <betatim@gmail.com>
2020-06-07 15:55:07 +0200

    Update token refresher
    Adjsuted the refresher to the new way of reading the JH config and
    switched the base image to debian instead of alpine to reduce the amount
    of pain from wheels not being available on alpine.

--- images/refresher/whoami.py
+++ images/refresher/whoami.py
@@ -1,5 +1,7 @@
 import json
 import os
+from collections import Mapping
+from functools import lru_cache
 from urllib.parse import urlencode, urlparse
 
 import requests
@@ -17,18 +19,59 @@ from jupyterhub.services.auth import HubAuthenticated
 from jupyterhub.utils import url_path_join
 
 
+# memoize so we only load config once
+@lru_cache()
+def _load_config():
+    """Load configuration from disk
+    Memoized to only load once
+    """
+    cfg = {}
+    for source in ('config', 'secret'):
+        path = f"/etc/jupyterhub/{source}/values.yaml"
+        if os.path.exists(path):
+            print(f"Loading {path}")
+            with open(path) as f:
+                values = yaml.safe_load(f)
+            cfg = _merge_dictionaries(cfg, values)
+        else:
+            print(f"No config at {path}")
+    return cfg
+
+
+def _merge_dictionaries(a, b):
+    """Merge two dictionaries recursively.
+    Simplified From https://stackoverflow.com/a/7205107
+    """
+    merged = a.copy()
+    for key in b:
+        if key in a:
+            if isinstance(a[key], Mapping) and isinstance(b[key], Mapping):
+                merged[key] = _merge_dictionaries(a[key], b[key])
+            else:
+                merged[key] = b[key]
+        else:
+            merged[key] = b[key]
+    return merged
+
+
 def get_config(key, default=None):
     """
     Find a config item of a given name & return it
     Parses everything as YAML, so lists and dicts are available too
+    get_config("a.b.c") returns config['a']['b']['c']
     """
-    path = os.path.join('/etc/jupyterhub/config', key)
-    try:
-        with open(path) as f:
-            data = yaml.safe_load(f)
-            return data
-    except FileNotFoundError:
-        return default
+    value = _load_config()
+    # resolve path in yaml
+    for level in key.split('.'):
+        if not isinstance(value, dict):
+            # a parent is a scalar or null,
+            # can't resolve full path
+            return default
+        if level not in value:
+            return default
+        else:
+            value = value[level]
+    return value
 
 
 async def fetch_new_token(token_url, client_id, client_secret, refresh_token):
